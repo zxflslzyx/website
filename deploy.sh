9de8ece375e00d7fbe4ed184017194595ca2cc3f
xiaofei19 <xiaofei19@staff.sina.com.cn>
2019-03-29 20:13:34 +0800

    dd

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,8 @@ WEB_USERGROUP='lovelucydev'
  
 echo "Start deployment"
 cd $WEB_PATH
-echo "pulling source code..."
-git reset --hard origin/master
-git clean -f
+echo "----------------pulling source code----------------------"
 git pull
-git checkout master
 echo "changing permissions..."
 chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
 echo "Finished."
