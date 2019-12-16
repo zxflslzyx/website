1f25e01f1d7800c690f9d510dd4346f506e9b55e
xiaofei19 <xiaofei19@staff.sina.com.cn>
2019-12-16 16:56:57 +0800

    jljlf

--- web/server.js
+++ web/server.js
@@ -4,6 +4,10 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, "./public"))).listen(80);
 
+app.get('/404', function (req, res) {
+    res.send('root')
+  })
+
 
 console.log('listen ok test test');
 console.log('listen ok test test');
