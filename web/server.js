ceb4cc108ae3ba347f5abba4f57ae0eef897b6ef
xiaofei19 <xiaofei19@staff.sina.com.cn>
2019-12-17 10:19:46 +0800

    modify server.js

--- web/server.js
+++ web/server.js
@@ -1,18 +1,11 @@
 var express = require("express");
 var app = express();
-var path = require('path');
+var path = require("path");
 
-
-console.log('listen ok test test');
-
+console.log("listen ok test test");
 
 app.use(express.static(path.join(__dirname, "./public"))).listen(80);
 
-app.get('/404', function (req, res) {
-    res.send('u r ok??')
-  })
-
-
-
-
-
+app.get("/404", function(req, res) {
+  res.send("WTF u r talkin' bout??");
+});
