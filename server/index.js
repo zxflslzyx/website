9be525d87537522202708b7e5c658687ef25711d
xiaofei19 <xiaofei19@staff.sina.com.cn>
2019-12-30 19:25:04 +0800

    automatically deal home summary

--- server/index.js
+++ server/index.js
@@ -1,17 +1,40 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var ejs = require("ejs");
+const fs = require("fs");
 
 console.log("listen ok test test");
 
+app.get("/home", function(req, res) {
+  fs.readdir(path.join(__dirname, "../web/public"), function(err, data) {
+    if (err) {
+      res.send("<h1>getDir error</h1>");
+    } else {
+      //  只取出 文件夹
+      data = data.filter(item => {
+        return !/\..*$/.test(item);
+      });
+
+      let linkArr = [];
+      data = data.map(item => {
+        let obj = {};
+        let jsonData = fs.readFileSync(path.join(__dirname, `../web/public/${item}/page.json`), "utf-8");
+        obj.name = JSON.parse(jsonData).name;
+        obj.url = `/${item}`;
+        linkArr.push(obj);
+      });
+
+      console.log(JSON.stringify(linkArr));
+
+      // 创建用于渲染的数据
+      let ejsData = {
+        data: linkArr
+      };
+      // 将数据在浏览器进行展现
+      res.render(path.join(__dirname, "./views/index/index.ejs"), ejsData);
+    }
+  });
+});
+
 app.use(express.static(path.join(__dirname, "../web/public"))).listen(80);
-
-
-
-app.get("/ejs", function(req, res) {
-  res.send("<h1>WTF u r talkin' boutf<h1>");
-});
-
-app.get("*", function(req, res) {
-  res.send("WTF u r talkin' bout??");
-});
