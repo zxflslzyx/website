12fe6bb86cef26e966a1a457abc51bf25bde73b4
xiaofei19 <xiaofei19@staff.sina.com.cn>
2020-04-20 20:09:29 +0800

    完善两端

--- server/websocket.js
+++ server/websocket.js
@@ -6,13 +6,30 @@ app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-io.emit("some event", { for: "everyone" });
+// io.emit("some event", { for: "everyone" });
+
+// io.on("connection", (socket) => {
+//   console.log("hhello");
+//   socket.on("sendMsg", (msg) => {
+//     console.log("[log]===> receive msg:", msg);
+//     io.emit("sendMsg", msg);
+//   });
+// });
+
+let signList = ["1587376316756"];
 
 io.on("connection", (socket) => {
-  console.log("hhello");
-  socket.on("sendMsg", (msg) => {
-    console.log("[log]===> receive msg:", msg);
-    io.emit("sendMsg", msg);
+  let onlineNum = io.eio.clientsCount;
+
+  io.emit("showOnlineNum", { num: onlineNum });
+
+  signList.forEach((item) => {
+    let eventName = `sendMsg_${item}`;
+
+    socket.on(eventName, (msg) => {
+      console.log(`[log]===> receive ${eventName} msg:`, msg);
+      io.emit(eventName, msg);
+    });
   });
 });
 
