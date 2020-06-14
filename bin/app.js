08dd788ebab0593cff34f872fe6cfab063ba1702
xiaofei19 <xiaofei19@staff.sina.com.cn>
2020-06-14 12:01:59 +0800

    update pages

--- bin/app.js
+++ bin/app.js
@@ -1,30 +1,31 @@
-// var http = require("http");
-// var spawn = require("child_process").spawn;
+var http = require("http");
+var spawn = require("child_process").spawn;
 
-// function run_cmd(cmd, args, callback) {
-//   var child = spawn(cmd, args);
-//   var resp = "";
+function run_cmd(cmd, args, callback) {
+  var child = spawn(cmd, args);
+  var resp = "";
 
-//   child.stdout.on("data", function (buffer) {
-//     resp += buffer.toString();
-//   });
-//   child.stdout.on("end", function () {
-//     callback(resp);
-//   });
+  child.stdout.on("data", function (buffer) {
+    resp += buffer.toString();
+  });
+  child.stdout.on("end", function () {
+    callback(resp);
+  });
 
-//   child.stdout.on('error', (err) => {
-//     console.error('启动子进程失败');
-//   });
-// }
+  child.stdout.on("error", (err) => {
+    console.error("启动子进程失败");
+  });
+}
 
-// run_cmd("sh", ["./setup.sh"], function (text) {
-//   console.log(text);
-// });
-
-
-const { spawn } = require('child_process');
-const subprocess = spawn('错误的命令');
-
-subprocess.on('error', (err) => {
-  console.error('启动子进程失败');
+run_cmd("sh", ["./setup.sh"], function (text) {
+  console.log(text);
 });
+
+console.log("exec ./bin/app.js...");
+
+// const { spawn } = require('child_process');
+// const subprocess = spawn('错误的命令');
+
+// subprocess.on('error', (err) => {
+//   console.error('启动子进程失败');
+// });
